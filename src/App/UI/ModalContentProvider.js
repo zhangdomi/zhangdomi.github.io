e497afc4fd33aaa62eeb0916dca8e5a0bc7963ac
zhangdomi <zhangyj2023@gmail.com>
2024-05-28 11:10:56 +0800

    corrected some thigns

--- src/App/UI/ModalContentProvider.js
+++ src/App/UI/ModalContentProvider.js
@@ -3,7 +3,7 @@ export default class ModalContentProvider {
       this.modalContents = {
         aboutMe: {
           title: 'About me',
-          description: 'I am a rising sophomore at the University of Michigan, where I study Math and Computer Science. <br> <a href="https://www.linkedin.com/in/zhang-yu-jie/" target="_blank" rel="noopener noreferrer">LinkedIn</a>',
+          description: 'I\'m a rising sophomore at the University of Michigan, where I study Math & CS. <br> I\'m also the <i>Co-President</i> of Michigan Finance & Math Society, <b>MFAMS</b>  <br> <a href="https://www.linkedin.com/in/zhang-yu-jie/" target="_blank" rel="noopener noreferrer">LinkedIn</a>',
         },
         projects: {
           title: 'Projects',
@@ -11,11 +11,11 @@ export default class ModalContentProvider {
         },
         contactMe: {
           title: 'Course Reviews',
-          description: 'A biased review of UMich classes, compiled by a Math & CS kid (me). <br> <a href="https://docs.google.com/document/d/11CwAcdEzU7ltuhSxzo_O3B8DOsW7xKYAmeB84HG73z8/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Course Reviews</a>',
+          description: 'A <b>biased review</b> of UMich classes, compiled by a me. <br> <a href="https://docs.google.com/document/d/11CwAcdEzU7ltuhSxzo_O3B8DOsW7xKYAmeB84HG73z8/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Course Reviews</a>',
         },
         quant:{
           title: 'Quant Resources',
-          description: 'I am currently recruiting for quantitative finance and software engineering internships, as the Co-President of Michigan Finance and Math Society, I compiled some resources. <br> Find it <a href="https://docs.google.com/document/d/1H33p5LC6DV-jEm25BaBhmULfhvCAgZbggin_DZYorDc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">here</a>. '
+          description: 'I am currently recruiting for quantitative finance and software engineering internships, so I compiled some resources mainly for MFAMS but I made it available for everyone. <br> Find it <a href="https://docs.google.com/document/d/1H33p5LC6DV-jEm25BaBhmULfhvCAgZbggin_DZYorDc/edit?usp=sharing" target="_blank" rel="noopener noreferrer">here</a>. '
         }
       }
     }
